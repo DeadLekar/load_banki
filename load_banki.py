555d3174d6cfb962eae0819e97f79c4e79d8e1b5
DeadLekar <vladkovalenkoreg@mail.ru>
2020-04-26 07:44:45 +0300

    initial

---
+++ load_banki.py
@@ -0,0 +1,61 @@
+import requests
+import sqlite3 as lite
+import time
+
+class NewsGetter:
+    conn = None
+    c = None
+
+    def __init__(self, _conn):
+        self.conn = _conn
+        self.c = self.conn.cursor()
+
+    def get_news(self, month_num):
+        pass
+
+
+class LinksSeeker:
+    conn = None
+    c = None
+
+    def __init__(self, _conn):
+        self.conn = _conn
+        self.c = self.conn.cursor()
+
+    def get_links(self):
+        links_to_seek_rows = self.c.execute("SELECT link FROM links WHERE links_cnt ISNULL").fetchall()
+        for row in links_to_seek_rows:
+            link = row[0]
+            r = requests.get(link)
+            start_pos = r.text.find("article-text")
+            end_pos = r.text.find("/article", start_pos)
+            text = r.text[start_pos:end_pos]
+
+            # look for links
+            cnt_all_links = text.count("www.banki.ru")
+            cnt_news = text.count("banki.ru/news")
+            cnt_not_news = cnt_all_links - cnt_news
+
+            # look for widgets
+            cnt_widgets_new = text.count("header-widget")
+            cnt_widgets_old = text.count("BankiruNewsBundle:Widget")
+
+            # update db
+            self.c.execute("UPDATE links SET links_cnt={} WHERE link='{}'".format(cnt_not_news, link))
+            self.c.execute("UPDATE links SET new_widgets_cnt={} WHERE link='{}'".format(cnt_widgets_new, link))
+            self.c.execute("UPDATE links SET old_widgets_cnt={} WHERE link='{}'".format(cnt_widgets_old, link))
+            conn.commit()
+            print(link, cnt_not_news, cnt_widgets_new, cnt_widgets_old)
+            # time.sleep(1)
+
+
+db_path = "C:/my_folder/bases/banki/articles.db"
+conn = lite.connect(db_path)
+# seeker = LinksSeeker(conn)
+news_getter = NewsGetter(conn)
+news_getter.get_news('04')
+
+
+
+
+
